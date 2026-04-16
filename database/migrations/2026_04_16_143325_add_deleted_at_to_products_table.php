57d92ce0178a5e294258b0f067d47d23907d2019
Evaristo <tu-email@ejemplo.com>
2026-04-16 11:35:36 -0300

    fix metricas dashboard clinic manager

--- database/migrations/2026_04_16_143325_add_deleted_at_to_products_table.php
+++ database/migrations/2026_04_16_143325_add_deleted_at_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
