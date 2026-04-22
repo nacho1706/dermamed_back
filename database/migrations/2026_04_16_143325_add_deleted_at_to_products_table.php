8d9ce3352e297e987a46523255777e127a5057a8
Corcho <evaristovillalobo@gmail.com>
2026-04-22 11:08:59 -0300

    fixx db

--- database/migrations/2026_04_16_143325_add_deleted_at_to_products_table.php
+++ database/migrations/2026_04_16_143325_add_deleted_at_to_products_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->softDeletes();
+            if (!Schema::hasColumn('products', 'deleted_at')) {
+                $table->softDeletes();
+            }
         });
     }
 
